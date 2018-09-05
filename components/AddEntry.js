c4fed102c20c5c4f49b8c722831ba8982c2771b7
Vinod Chandra <vinodchandra.net@gmail.com>
2018-09-05 13:59:04 -0400

    Changes for AddEntry-render

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
+import Slider from './Slider'
+import Steppers from './Steppers'
 
 export default class AddEntry extends Component {
   state = {
@@ -10,7 +12,6 @@ export default class AddEntry extends Component {
     sleep: 0,
     eat: 0,
   }
-
   increment = metric => {
     const { max, step } = getMetricMetaInfo(metric)
 
@@ -23,7 +24,6 @@ export default class AddEntry extends Component {
       }
     })
   }
-
   decrement = metric => {
     this.setState(state => {
       const count = state[metric] - getMetricMetaInfo(metric).step
@@ -34,14 +34,41 @@ export default class AddEntry extends Component {
       }
     })
   }
-
   slide = (metric, value) => {
     this.setState(() => ({
       [metric]: value,
     }))
   }
-
   render() {
-    return <View />
+    const metaInfo = getMetricMetaInfo()
+
+    return (
+      <View>
+        {Object.keys(metaInfo).map(key => {
+          const { getIcon, type, ...rest } = metaInfo[key]
+          const value = this.state[key]
+
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider' ? (
+                <Slider
+                  value={value}
+                  onChange={value => this.slide(key, value)}
+                  {...rest}
+                />
+              ) : (
+                <Steppers
+                  value={value}
+                  onIncrement={() => this.increment(key)}
+                  onDecrement={() => this.decrement(key)}
+                  {...rest}
+                />
+              )}
+            </View>
+          )
+        })}
+      </View>
+    )
   }
 }
