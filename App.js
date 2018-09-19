da6489d718c32e8add8a240f4db09470e999ee96
VINOD CHANDRA <vinodchandra.net@gmail.com>
2018-09-18 22:41:58 -0400

    StackNavigator v2 implementation

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-import { createBottomTabNavigator, StackNavigator } from 'react-navigation'
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+} from 'react-navigation'
 import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
@@ -55,9 +58,12 @@ const Tabs = createBottomTabNavigator(
   },
 )
 
-const MainNavigator = StackNavigator({
+const MainNavigator = createStackNavigator({
   Home: {
     screen: Tabs,
+    navigationOptions: {
+      header: null,
+    },
   },
   EntryDetail: {
     screen: EntryDetail,
